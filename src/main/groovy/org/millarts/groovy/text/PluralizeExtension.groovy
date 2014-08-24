2e7a4e23c7d51edccf6e25ed84caa21a38910aa5
David W Millar <david.w.millar@gmail.com>
2014-08-24 18:42:18 -0400

    Attempted deploy and signing with maven-publish plugin. Deploying signatures with this configuration is either difficult or impossible. Falling back to maven plugin.

--- src/main/groovy/org/millarts/groovy/text/PluralizeExtension.groovy
+++ src/main/groovy/org/millarts/groovy/text/PluralizeExtension.groovy
@@ -5,6 +5,8 @@ import org.atteo.evo.inflector.English
 /**
  * This class enhances strings by adding a plural() method
  * that returns the plural inflection for a word.
+ *
+ * @since 0.1.0
  */
 class PluralizeExtension {
   static String plural(final String self, int cardinality = 2) {
